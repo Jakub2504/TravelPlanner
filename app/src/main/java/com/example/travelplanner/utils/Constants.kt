b7f831252c38b9fcf67ca9f20297ec516a792fa3
Jakub2504 <wysockij25@gmail.com>
2025-03-22 03:37:25 +0100

    Final

--- app/src/main/java/com/example/travelplanner/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travelplanner.utils
-
-object Constants {
-    const val BASE_URL = "https://api.travelplanner.com/"
-}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+    }
+}
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
